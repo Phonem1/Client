1c21a4ef6a662042e104517e196a09d7f3b16d91
Chu Guan Zhi <alvin_0897@hotmail.com>
2017-12-26 18:32:24 +0800

    Added Splash Screen, Trying to make it show Splash when app is idle(Incomplete)

--- app/src/main/java/com/anewtech/phone/client/MainActivity.java
+++ app/src/main/java/com/anewtech/phone/client/MainActivity.java
@@ -1,13 +1,25 @@
 package com.anewtech.phone.client;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+Handler handler;
+Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        runnable = new runnable() {
+            public void run() {
+                Intent intent = new Intent(this, SplashScreen.class);
+
+                startActivity(intent);
+            }
+        };
+        handler.postDelayed(runnable,6000);
     }
 }
