66f0c7d08ae50e8ef61691c7d4844bac21d98070
Chu Guan Zhi <alvin_0897@hotmail.com>
2017-12-27 18:27:53 +0800

    splash screen idling in the app for 1 min will make it go back to splashscreen touching of screen will redirect to menu screen(Incompete, error currently)

--- app/src/main/java/com/anewtech/phone/client/SplashScreen.java
+++ app/src/main/java/com/anewtech/phone/client/SplashScreen.java
@@ -3,15 +3,25 @@ package com.anewtech.phone.client;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 
 public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        RelativeLayout splash =  findViewById(R.id.splashlayout);
+        splash.setOnClickListener(new View.OnClickListener() {
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        });
+
     }
 }
